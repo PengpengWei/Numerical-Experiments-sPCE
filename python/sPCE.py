8a338917ee3d2627f5e1229118a60a6f8e30ce22
Xupeng Wei <30372215+PengpengWei@users.noreply.github.com>
2023-03-23 09:14:44 -0400

    Check if the sPCE is also sPBE

--- python/sPCE.py
+++ python/sPCE.py
@@ -1,11 +1,12 @@
 import numpy as np
 from scipy.optimize import linprog
 import time
+import pickle
 import matplotlib.pyplot as plt
 
 
 class PublicInvestProblem:
-    def __init__(self, q_vals=0.1, x_vals=[0.2, 1.2], num_quant=2, T=2, tol_dig=8):
+    def __init__(self, q_vals=0.1, x_vals=[0.2, 1.2], num_quant=2, T=2, tol_dig=8, filename=None):
         """Initialize a public investment problem instance. 
 
         Args:
@@ -20,11 +21,17 @@ class PublicInvestProblem:
 
         :tol_dig: tolerant digit: set tolerance of belief to be 10^(-tol_dig). tol_dig = 8 by default. Set tol_dig=-1 if not needed.
 
+        :filename: initialize the object using a saved file "filename.pkl". None by default.
+
         Returns:
         ---
         None.
 
         """
+        if filename: 
+            self.load(filename)
+            return 
+        
         # Read the input vars
         self.q_vals = np.array(q_vals).reshape(-1, ).flatten()
         self.x_vals = np.array(x_vals).reshape(-1, ).flatten()
@@ -34,9 +41,9 @@ class PublicInvestProblem:
 
         # Initialize other vars.
         # Instantaneous reward.
-        self.reward = lambda i, x0, x1, a0, a1: \
-            (a0 + a1 > 0) * 1 - (i == 0 and a0 == 1) * self.x_vals[x0] - \
-                (i == 1 and a1 == 1) * self.x_vals[x1]
+        # self.reward = lambda i, x0, x1, a0, a1: \
+        #     (a0 + a1 > 0) * 1 - (i == 0 and a0 == 1) * self.x_vals[x0] - \
+        #         (i == 1 and a1 == 1) * self.x_vals[x1]
 
         # A list of possible gamma values, ranging from 0 to 1.
         self.gamma_vals = np.linspace(0, 1, num_quant)
@@ -51,16 +58,27 @@ class PublicInvestProblem:
                               self.num_quant, self.num_quant)) for t in range(self.T)]
         # self.V[t] is the value to go function. Format: self.V[t][i, pi0_ind, pi1_ind, xi] (note that t can be T.)
         self.V = [np.zeros((2, len(self.pi_vals[t]), len(self.pi_vals[t]), 2)) for t in range(T + 1)]
-        # self.social_welfare is the social welfare to go. Format: self.social_welfare[t][pi0_ind, pi1_ind].
+        # self.social_welfare is the social welfare to go. 
+        # Format: self.social_welfare[t][pi0_ind, pi1_ind].
+        # Exception: index 0: 1-d: each pi_ind represents pi0_ind = pi1_ind = pi_ind.
         self.social_welfare = [np.zeros((len(self.pi_vals[t]), len(self.pi_vals[t]))) for t in range(T + 1)]
 
         self.__complete = False # A var that indicates if the solver has been run or not.
         self.__team = False # A var that indicates if the solver has been run under team mode or not.
+        self.__is_compared = False # A var that indicates if the compare() has been run or not.
+
+        # For sPBE check.
+        self.__checked_sPBE = [False] * self.T
+        # self.sPBE_flag indicates if the device starts from the current belief is an sPBE. 
+        # Format: self.sPBE_flag[t][pi0_ind, pi1_ind].
+        # Exception: index 0: 1-d: each pi_ind represents pi0_ind = pi1_ind = pi_ind.
+        self.sPBE_flag = [np.full((len(self.pi_vals[t]), len(self.pi_vals[t])), True) for t in range(self.T)]
 
         # For comparison purpose.
         self.phi_comp = self.phi
         self.V_comp = self.V
         self.social_welfare_comp = self.social_welfare
+        self.sPBE_flag_comp = self.sPBE_flag
 
         return
     
@@ -91,6 +109,61 @@ class PublicInvestProblem:
         if type(tol_dig) == type(None): tol_dig = self.tol_dig
         self.__init__(q_vals=q_vals, x_vals=x_vals, num_quant=num_quant, T=T, tol_dig=tol_dig)
         return
+
+    def save(self, filename=None):
+        """Save the current environment.
+
+        Args:
+        ---
+        :filename: a file name. None by default. The environment will be saved as "filename".pkl in the current path.
+        If filename is not given, it will use the current time.
+
+        Returns:
+        ---
+        None.
+        """
+        if filename == None: filename = time.ctime()
+        with open(filename + '.pkl', 'wb') as f:
+            pickle.dump(self.__dict__, f, pickle.HIGHEST_PROTOCOL)
+        return
+    
+    def load(self, filename):
+        """Load a saved environment. This method will overwrite the current object.
+
+        Args:
+        ---
+        :filename: a filename without ".pkl".
+
+        Returns:
+        ---
+        None, but the current object will be replaced by the loaded object if success.
+        """
+        with open(filename + ".pkl", 'rb') as f:
+            tmp_dict = pickle.load(f)
+        
+        self.__dict__.clear()
+        self.__dict__.update(tmp_dict)
+
+        return
+    
+    def reward(self, i, x0, x1, a0, a1):
+        """Calculate the instantaneous reward.
+
+        Args:
+        ---
+        :i: player's index.
+        :x0: player 0's state.
+        :x1: player 1's state.
+        :a0: player 0's action.
+        :a1: player 1's action.
+
+        Returns:
+        ---
+        The instantaneous reward of player i given x0, x1, a0, a1.
+        """
+        return (a0 + a1 > 0) * 1 - (i == 0 and a0 == 1) * self.x_vals[x0] - \
+                (i == 1 and a1 == 1) * self.x_vals[x1]
+            
 
     def __find_belief_candidate(self):
         """Calculate all the possible pi^i_t values given the environment settings.
@@ -485,6 +558,9 @@ class PublicInvestProblem:
                 print()
                 self.status(t, fig)
 
+        if not team:
+            self.is_sPBE()
+
         if verbose:
             main_end = time.time()
             print("Total runtime: {} seconds".format(main_end - main_start))
@@ -576,6 +652,7 @@ class PublicInvestProblem:
 
         if self.__team: 
             social_team, social_game = self.social_welfare[0].flatten(), self.social_welfare_comp[0].flatten()
+            self.sPBE_flag_comp = another.sPBE_flag
         else:
             social_game, social_team = self.social_welfare[0].flatten(), self.social_welfare_comp[0].flatten()
         plt.plot(self.q_vals, social_game, label = "Social welfare: sPCE")
@@ -585,15 +662,150 @@ class PublicInvestProblem:
         plt.legend()
         plt.show()
 
+        self.__is_compared = True
+
         return
     
-    # def test(self):
-    #     res = self.__single_row_rational_to_substract(ind_list=np.array([1, 2, 1, 1, 0, 3, 4]), t=1).flatten()
-    #     return res
+    def is_sPBE(self, t=0):
+        """Check if a device at time t is sPBE or not for every given (pi1_ind, pi2_ind).
+
+        Args:
+        ---
+        :t: time t of your interest.
+
+        Returns:
+        ---
+        None.
+
+        Update the self.sPBE_flag[t:] and self.__checked_sPBE[t:] as well.
+        """
+        if t >= self.T or t < 0: return
+        if self.__checked_sPBE[t]: return 
+        if t != self.T - 1 and self.__checked_sPBE[t + 1] == False: self.is_sPBE(t + 1)
+
+        def is_next_sPBE(inputs):
+            """(Local) Check if the phi_{t+1}[next_pi] is an sPBE or not.
+
+            Args:
+            ---
+            :inputs: format: [pi0_ind, pi1_ind]
+
+            Returns:
+            ---
+            True or False
+            """
+            pi0_ind, pi1_ind = inputs
+            zero_bound = (self.tol_dig > 0) * (10 ** (-self.tol_dig))           
+            # ind: g0L_ind, g0H_ind, g1L_ind, g1H_ind, a0, a1.
+            ga_inds = np.indices((self.num_quant, self.num_quant, self.num_quant, self.num_quant, 2, 2)) 
+
+            # For each (gamma, a) tuple, first check if gamma has nonzero prob. If not, we don't have to check sPBE for this tuple.
+            is_0_measure = \
+                lambda g_a_ind: \
+                    (self.phi[t][pi0_ind, pi1_ind, g_a_ind[0], g_a_ind[1], g_a_ind[2], g_a_ind[3]] < zero_bound).flatten()
+            # For (gamma, a) whose gamma can be recommended, check if the next phi adopts sPBE.
+            if_next_is_sPBE = lambda g_a_ind: self.sPBE_flag[t+1][self.next_belief_ind(t, pi0_ind, g_a_ind[0], g_a_ind[1], g_a_ind[4]), \
+                                                       self.next_belief_ind(t, pi1_ind, g_a_ind[2], g_a_ind[3], g_a_ind[5])].flatten()
+            
+            # There are two cases of sPBE: 1. gamma not taken at all, 2. gamma taken, but the next is sPBE.
+            agg_ftn = lambda g_a_ind: np.logical_or(is_0_measure(g_a_ind), if_next_is_sPBE(g_a_ind))
+            return np.apply_along_axis(agg_ftn, axis=0, arr=ga_inds).all()
+
+
+        ind = np.indices((len(self.pi_vals[t]), len(self.pi_vals[t])))
+        temp_ftn = lambda x: is_indep(self.phi[t][x[0], x[1], :, :, :, :].reshape(self.num_quant ** 2, self.num_quant ** 2))
+        self.sPBE_flag[t] = np.apply_along_axis(temp_ftn, axis=0, arr=ind)
+
+        if t < self.T - 1:
+            self.sPBE_flag[t] = np.logical_and(self.sPBE_flag[t], \
+                                                np.apply_along_axis(is_next_sPBE, axis=0, arr=ind))
+            
+        if t == 0:
+            self.sPBE_flag[t] = np.diagonal(self.sPBE_flag[t])
+
+        self.__checked_sPBE[t] = True
+
+        return
+
+
+    def detailed_plot(self, filename=None):
+        """Plot a detailed figure of experiment results.
+        It will do nothing if self.compare() hasn't been done.
+
+        Args:
+        ---
+        :filename: Save the figure to a file named `filename`. None by default.
+
+        Returns:
+        ---
+        None. Plot a figure.
+        """        
+        if not self.__is_compared: 
+            print("Please run compare() first.")
+            print()
+            return
+        
+        if self.__team: 
+            social_team, social_game = self.social_welfare[0].flatten(), self.social_welfare_comp[0].flatten()
+            sPBE_markers = self.sPBE_flag_comp[0]
+        else:
+            social_game, social_team = self.social_welfare[0].flatten(), self.social_welfare_comp[0].flatten()
+            sPBE_markers = self.sPBE_flag[0]
+        plt.plot(self.q_vals, social_game, label = "Social welfare: sPCE")
+        plt.plot(self.q_vals, social_team, label = "Social welfare: team")
+        # plt.scatter(self.q_vals[sPBE_markers], social_game[sPBE_markers],\
+        #              color="green", marker="x")
+        # plt.scatter(self.q_vals[np.logical_not(sPBE_markers)], social_game[np.logical_not(sPBE_markers)],\
+        #              color="red", marker="x")
+
+        # Mark the sPBE and sPCE by fill_between.
+        plt.fill_between(self.q_vals, social_game, np.zeros(social_game.shape), where=np.logical_not(np.isnan(social_game)), \
+                         color='green', alpha=0.3, label="sPCE")
+        # plt.fill_between(self.q_vals, social_game, np.zeros(social_game.shape), where=np.logical_not(sPBE_markers),\
+        #                  color='red', alpha=0.3, label="sPCE")
+        plt.fill_between(self.q_vals, social_game, np.zeros(social_game.shape), where=sPBE_markers, \
+                         color='red', alpha=0.3, label="sPBE")
+        
+
+        plt.xlabel("Initial q values")
+        plt.ylabel("Social welfare")
+        plt.legend()
+        if filename:
+            plt.savefig(filename)
+        plt.show()
+
+        return
+
+
+
+def is_indep(joint_dist, tolerant=1e-8):
+    """Check if two random variables with the given joint distribution are independent or not.
+
+    Args:
+    ---
+    :joint_dist: a 2 dimensional matrix. The joint distribution of two random variables.
+
+    :tolerant: Tolerant. 1e-8 by default.
+
+    Returns:
+    ---
+    :res: True if independent; False if not. 
+    """
+    if np.isnan(joint_dist).any(): return False
+
+    x_margin = np.sum(joint_dist, axis=1).reshape(-1, 1)
+    y_margin = np.sum(joint_dist, axis=0).reshape(1, -1)
+
+    if (np.abs(joint_dist - x_margin * y_margin) < tolerant).all(): return True
+    return False
+
 
 
 if __name__ == "__main__":
-    env = PublicInvestProblem(q_vals=0.3, num_quant=3, T=2)
-    env.test()
+    # env = PublicInvestProblem(q_vals=np.arange(0.01, 1, 0.01), num_quant=2, T=2)
+    # env.compare()
+    # env.save("0322")
+    env = PublicInvestProblem(filename="0322")
+    env.detailed_plot("0322.pdf")
 
     print()
